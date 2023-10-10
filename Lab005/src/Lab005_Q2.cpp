3550d6fd13b4dbfff6e3312d422de5203fbe4441
JECunanan <jecunanan.learning@gmail.com>
2023-10-10 10:47:11 +0800

    Lab005_Q2 answered

--- Lab005/src/Lab005_Q2.cpp
+++ Lab005/src/Lab005_Q2.cpp
@@ -28,7 +28,39 @@ int main() {
     // Place your code logic after this comment line
     // ***********************************************************
 
+    // Variable declaration
+    int userInput;
 
+    // Ask user for input
+    cout << "Please enter an integer." << endl;
+    cin >> userInput;
+
+    // Switch Statements
+    switch (userInput) {
+        case 0:
+            cout << "Hello World" << endl;
+            break;
+
+        case 1:
+            cout << "I am good" << endl;
+            break;
+
+        case 2:
+            cout << "To the Top" << endl;
+            break;
+
+        case 3:
+            cout << "Where is the horizon" << endl;
+            break;
+
+        case 4:
+            cout << "I do not know" << endl;
+            break;
+
+        default:
+            cout << "Yeah, I will." << endl;
+        
+    }
 
     // ********************** DO NOT CHANGE **********************
     // Print a new line and ask user for any key before exiting
