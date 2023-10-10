ba8810ba7dcb81cc0f69e0a8c872ba0730726a90
JECunanan <jecunanan.learning@gmail.com>
2023-10-10 10:24:18 +0800

    file init

---
+++ Lab005/src/Lab005_Q1.cpp
@@ -0,0 +1,38 @@
+/* ****************************************************************
+ * @author: <J. E. Cunanan>
+ * @student number: <2022-0072254>
+ * @app name: <Hello World>
+ * @app desc: <C++ Learning>
+ * @history:
+ *  - <2023/10/10> | <Lab005 Question 1>
+ *      -- Answered Question 1 of Lab005
+ * ****************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include "_pause.h"
+
+using namespace std;
+
+//////////////////////////////////////////////////////////////////
+//                               NOTE
+// This is your program entry point. Your main logic is placed
+// inside this function. You may add your functions before this
+// "main()", or after this "main()" provided you added reference
+// before this "main()" function.
+//////////////////////////////////////////////////////////////////
+
+int main() {
+    // ************************** TO DO **************************
+    // Place your code logic after this comment line
+    // ***********************************************************
+
+
+
+    // ********************** DO NOT CHANGE **********************
+    // Print a new line and ask user for any key before exiting
+    // ***********************************************************
+    _pause();
+    return EXIT_SUCCESS;
+}
