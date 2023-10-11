3f6e72c2a5ffc79def0193736c3181dc55d50178
JECunanan <jecunanan.learning@gmail.com>
2023-10-11 15:41:16 +0800

    Lab006_Q2 answered

--- Lab006/src/Lab006_Q2.cpp
+++ Lab006/src/Lab006_Q2.cpp
@@ -5,7 +5,7 @@
  * @app desc: <C++ Learning>
  * @history:
  *  - <2023/10/10> | <Lab006 Question 2>
- *      -- Answered Question 2 of Lab002
+ *      -- Answered Question 2 of Lab006
  * ****************************************************************/
 
 #include <cstdio>
@@ -17,10 +17,34 @@ using namespace std;
 
 int main() {
 
+    // Variable declaration and initialization
+    int height;
+    int i = 1;
 
+    // Ask user for input
+    cout << "Please enter the height of the right triangle." << endl;
+    cin >> height;
 
+    // FOR loop
+    for (int i = 1; i <= height; i++) {
 
+        // Spaces
+        for (int j = height; j > i; j--) {
 
+            cout << " ";
+
+        }
+
+        // Asterisks
+        for (int k = 1; k <= i; k++) {
+            
+            cout << "*";
+
+        }
+        
+        cout << "\n";
+    }
+    
     _pause();
     return EXIT_SUCCESS;
 }
