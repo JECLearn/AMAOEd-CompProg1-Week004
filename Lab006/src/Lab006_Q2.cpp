82028908a26e582339618f7aacd80c82dbe8d022
JECunanan <jecunanan.learning@gmail.com>
2023-10-11 14:59:04 +0800

    Lab006 files init

---
+++ Lab006/src/Lab006_Q2.cpp
@@ -0,0 +1,26 @@
+/* ****************************************************************
+ * @author: <J. E. Cunanan>
+ * @student number: <2022-0072254>
+ * @app name: <Lab 006>
+ * @app desc: <C++ Learning>
+ * @history:
+ *  - <2023/10/10> | <Lab006 Question 1>
+ *      -- Answered Question 1 of Lab005
+ * ****************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include "_pause.h"
+
+using namespace std;
+
+int main() {
+
+
+
+
+
+    _pause();
+    return EXIT_SUCCESS;
+}
