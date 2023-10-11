84d91736bae7d80bf0d77acbf5597cf4103e8e01
JECunanan <jecunanan.learning@gmail.com>
2023-10-11 18:18:56 +0800

    Lab007_Q3 answered

--- Lab007/src/Lab007_Q3.cpp
+++ Lab007/src/Lab007_Q3.cpp
@@ -17,6 +17,39 @@ using namespace std;
 
 int main() {
 
+    // Variable declaration and initialization
+    const int arraySize = 6;
+    int numbers[arraySize];
+
+    cout << "Please enter six integers:\n";
+
+    // Input integers into the array
+    for (int i = 0; i < arraySize; i++) {
+        cout << "Enter integer " << (i + 1) << ": ";
+        cin >> numbers[i];
+    }
+    cout << "\n";
+
+    // Bubble Sort to sort the integers in ascending order
+    for (int i = 0; i < arraySize - 1; i++) {
+        for (int j = 0; j < arraySize - i - 1; j++) {
+            if (numbers[j] > numbers[j + 1]) {
+                // Swap numbers[j] and numbers[j + 1]
+                int temp = numbers[j];
+                numbers[j] = numbers[j + 1];
+                numbers[j + 1] = temp;
+            }
+        }
+    }
+
+    cout << "Sorted integers in ascending order:\n";
+    cout << "\n";
+
+    // Display the sorted integers
+    for (int i = 0; i < arraySize; i++) {
+        cout << numbers[i] << " ";
+    }
+    cout << "\n" << endl;
 
     _pause();
     return EXIT_SUCCESS;
