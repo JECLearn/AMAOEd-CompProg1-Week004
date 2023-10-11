0e6d9eee628390f40bff965dc3388240bcfbbb8c
JECunanan <jecunanan.learning@gmail.com>
2023-10-11 16:56:49 +0800

    Lab007 files init

---
+++ Lab007/src/Lab007_Q2.cpp
@@ -0,0 +1,23 @@
+/* ****************************************************************
+ * @author: <J. E. Cunanan>
+ * @student number: <2022-0072254>
+ * @app name: <Lab 007>
+ * @app desc: <AMA C++ Learning>
+ * @history:
+ *  - <Oct 11 2023> | <Lab007 Question 2>
+ *      -- Answered Question 2 of Lab007
+ * ****************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include "_pause.h"
+
+using namespace std;
+
+int main() {
+
+
+    _pause();
+    return EXIT_SUCCESS;
+}
