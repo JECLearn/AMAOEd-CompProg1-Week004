3d0a1d061bca0dad226de78608e404eb404d5c98
JECunanan <jecunanan.learning@gmail.com>
2023-10-11 17:37:35 +0800

    Lab007_Q2 answered

--- Lab007/src/Lab007_Q2.cpp
+++ Lab007/src/Lab007_Q2.cpp
@@ -16,7 +16,40 @@
 using namespace std;
 
 int main() {
+    
+    // Variable declaration and initialization
+    char characters[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    int height;
+    int characterIndex = 0;
 
+    // Ask user for input
+    cout << "Please enter a value between 1 and 10 to be the height of the triangle: ";
+    cin >> height;
+    cout << "\n";
+
+    // Implement input value limit
+    if (height < 1 || height > 10) {
+        cout << "Invalid. Please enter a height between 1 and 10 only.\n";
+        return 1;
+    }
+
+    // FOR loop
+    for (int i = 1; i <= height; i++) {
+        // Spaces
+        for (int j = 1; j <= height - i; j++) {
+            cout << " ";
+        }
+
+        // Characters
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            cout << characters[characterIndex];
+            // Wrapping to enforce value limit of 10
+            characterIndex = (characterIndex + 1) % 10;
+        }
+
+        cout << "\n";
+    }
+    cout << "\n";
 
     _pause();
     return EXIT_SUCCESS;
