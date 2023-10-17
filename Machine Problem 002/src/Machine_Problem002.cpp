f9eca356d9ddaeb629a2c4e21d08ebe443f8c272
JECunanan <jecunanan.learning@gmail.com>
2023-10-17 10:56:31 +0800

    Machine_Problem002 init

---
+++ Machine Problem 002/src/Machine_Problem002.cpp
@@ -0,0 +1,19 @@
+/* ****************************************************************
+ * @author: <J. E. Cunanan>
+ * @student number: <2022-0072254>
+ * @app name: <Machine Problem 2>
+ * @app desc: <C++ Learning>
+ * @history:
+ *  - <2023/10/10> | <Machine Problem 2>
+ *      -- 
+ * ****************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+
+int main() {
+
+    return 0;
+}
