f4f91e3612d5bf8bcdd36c99ffd2a098939c560e
JECunanan <jecunanan.learning@gmail.com>
2023-10-17 15:42:14 +0800

    Lab008 files init

---
+++ Lab008/src/Lab008_Q1.cpp
@@ -0,0 +1,22 @@
+/* ****************************************************************
+ * @author: <J. E. Cunanan>
+ * @student number: <2022-0072254>
+ * @app name: <Lab 008>
+ * @app desc: <AMA C++ Learning>
+ * @history:
+ *  - <Oct 17 2023> | <Lab008 Question 1>
+ *      -- Answered Question 1 of Lab008
+ * ****************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include "_pause.h"
+
+using namespace std;
+
+int main() {
+
+    _pause();
+    return EXIT_SUCCESS;
+}
