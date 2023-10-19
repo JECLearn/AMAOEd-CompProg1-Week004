7f01e0bcc108e25d4720299f9767869d738d4c29
JECunanan <jecunanan.learning@gmail.com>
2023-10-19 08:27:49 +0800

    Lab009 init

---
+++ Lab009/src/Lab009.cpp
@@ -0,0 +1,14 @@
+/* ****************************************************************
+ * @author: <J. E. Cunanan>
+ * @student number: <2022-0072254>
+ * @app name: <Lab 008>
+ * @app desc: <AMA C++ Learning>
+ * @history:
+ *  - <Oct 19 2023> | <Lab009 Questions>
+ *      -- Answered Questions 1, 2, 3 of Lab009
+ * ****************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
