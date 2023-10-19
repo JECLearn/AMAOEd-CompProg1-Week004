4fa87ac05257cd80c10791b398d12fb7567b3e94
JECunanan <jecunanan.learning@gmail.com>
2023-10-19 13:31:18 +0800

    Lab010_Q2 init

---
+++ Lab010/src/Lab010_Q2.cpp
@@ -0,0 +1,20 @@
+/* ****************************************************************
+ * @author: <J. E. Cunanan>
+ * @student number: <2022-0072254>
+ * @app name: <Lab 010>
+ * @app desc: <AMA C++ Learning>
+ * @history:
+ *  - <Oct 19 2023> | <Lab010 Question 2>
+ *      -- Answered Question 2 of Lab010
+ * ****************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
