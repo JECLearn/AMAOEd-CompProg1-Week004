bbc57ed9291710e4fa520eb648af41970640ed58
JECunanan <jecunanan.learning@gmail.com>
2023-10-20 10:32:29 +0800

    Machine Problem 003 init

---
+++ Machine Problem 003/src/Machine_Problem003.cpp
@@ -0,0 +1,20 @@
+/* ****************************************************************
+ * @author: <J. E. Cunanan>
+ * @student number: <2022-0072254>
+ * @app name: <Machine Problem 3>
+ * @app desc: <C++ Learning>
+ * @history:
+ *  - <Oct 20 2023> | <Machine Problem 3>
+ *      -- 
+ * ****************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
