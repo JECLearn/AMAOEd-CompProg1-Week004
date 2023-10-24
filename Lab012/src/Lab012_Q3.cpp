fc6c6b2499c272459a2fab28cfda8a6f6acc4846
JECunanan <jecunanan.learning@gmail.com>
2023-10-24 13:05:50 +0800

    Lab012 Q3 answered

--- Lab012/src/Lab012_Q3.cpp
+++ Lab012/src/Lab012_Q3.cpp
@@ -16,6 +16,20 @@
 using namespace std;
 
 int main() {
+    // declare uninitialized string variable
+    string exclamation;
+
+    // display variable before update
+    cout << "Variable before update : " << exclamation << endl;
+    cout << "\n";
+
+    // use pointer to variable's memory address to initialize exclamation variable
+    string* stringPtr = &exclamation;
+    *stringPtr = "This is it!";
+
+    // display variable after update
+    cout << "Variable after update  : " << exclamation << endl;
+    cout << "\n";
 
     _pause();
     return EXIT_SUCCESS;
