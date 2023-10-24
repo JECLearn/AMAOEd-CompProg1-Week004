a7fb8b9dc501b555c0f54e79d82ac4566c7c8597
JECunanan <jecunanan.learning@gmail.com>
2023-10-24 14:09:52 +0800

    Project file init

---
+++ Course Project Part 2/src/CourseProject_Part2.cpp
@@ -0,0 +1,20 @@
+/* ****************************************************************
+ * @author: <J. E. Cunanan>
+ * @student number: <2022-0072254>
+ * @app name: <Course Project Part 2>
+ * @app desc: <C++ Learning>
+ * @history:
+ *  - <Oct 24 2023> | <Course Project Part 2>
+ *      -- 
+ * ****************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
