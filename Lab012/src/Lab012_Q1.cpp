15be33e4735fb34663525f17c12e52a916d328b7
JECunanan <jecunanan.learning@gmail.com>
2023-10-24 12:21:42 +0800

    Lab012 files init

---
+++ Lab012/src/Lab012_Q1.cpp
@@ -0,0 +1,22 @@
+/* ****************************************************************
+ * @author: <J. E. Cunanan>
+ * @student number: <2022-0072254>
+ * @app name: <Lab 010>
+ * @app desc: <AMA C++ Learning>
+ * @history:
+ *  - <Oct 24 2023> | <Lab012 Question 1>
+ *      -- Answered Question 1 of Lab012
+ * ****************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include "_pause.h"
+
+using namespace std;
+
+int main() {
+
+    _pause();
+    return EXIT_SUCCESS;
+}
---
+++ Lab012/src/Lab012_Q3.cpp
@@ -0,0 +1,22 @@
+/* ****************************************************************
+ * @author: <J. E. Cunanan>
+ * @student number: <2022-0072254>
+ * @app name: <Lab 010>
+ * @app desc: <AMA C++ Learning>
+ * @history:
+ *  - <Oct 24 2023> | <Lab012 Question 3>
+ *      -- Answered Question 3 of Lab012
+ * ****************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include "_pause.h"
+
+using namespace std;
+
+int main() {
+
+    _pause();
+    return EXIT_SUCCESS;
+}
