dfc308abc92767339fea9e27ba4e73f1d1763b3c
JECunanan <jecunanan.learning@gmail.com>
2023-10-24 10:49:02 +0800

    Lab011 Q2 answered

--- Lab011/src/Lab011_Q2.cpp
+++ Lab011/src/Lab011_Q2.cpp
@@ -4,17 +4,32 @@
  * @app name: <Lab 010>
  * @app desc: <AMA C++ Learning>
  * @history:
- *  - <Oct 23 2023> | <Lab011 Question 2>
+ *  - <Oct 24 2023> | <Lab011 Question 2>
  *      -- Answered Question 2 of Lab011
  * ****************************************************************/
 
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include "_pause.h"
 
 using namespace std;
 
 int main() {
+    // initialize character array
+    char charArray[10];
+    char* charPointer = charArray;
 
-    return 0;
+    // display values of charArray
+    cout << "charArray value: ";
+    for(int i = 0; i < 10; i++) {
+        cout << static_cast<int>(charArray[i]) << " ";
+    }
+    cout << "\n";
+
+    cout << "Logical address of charArray: " << static_cast<void*>(charArray) << endl;
+    cout << "Logical address of charPointer: " << static_cast<void*>(charPointer) << endl;
+    
+    _pause();
+    return EXIT_SUCCESS;
 }
